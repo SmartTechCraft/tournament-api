e81d1a9741159bdaded3fd0384005fcf99f6bf88
DennisKretz <unrealfaceit2020@gmail.com>
2023-02-20 22:37:01 +0100

    remove headers

--- main/routes/user.py
+++ main/routes/user.py
@@ -6,7 +6,7 @@ from auth.auth_handler import sign_jwt
 
 from db.mysql import sessionLocal, engine
 from db import crud
-from fastapi import Depends, APIRouter, HTTPException, Response
+from fastapi import Depends, APIRouter, HTTPException
 from sqlalchemy.orm import Session
 
 models.Base.metadata.create_all(bind=engine)
@@ -27,24 +27,21 @@ def create_user(user: schemas.UserCreate, db: Session=Depends(get_db)):
     return crud.create_user(db=db, user=user)
 
 @router.post('/login')
-async def user_login(response: Response, user: schemas.UserLogin, db: Session=Depends(get_db)):
+async def user_login(user: schemas.UserLogin, db: Session=Depends(get_db)):
     db_user = crud.get_user_by_username(db=db, username=user.username)
     
     if (db_user and crud.validate_user_password(db=db, username=user.username, password=user.password)):
-        response.headers["Access-Control-Allow-Origin"] = '*'
         return sign_jwt(user.username)
     raise HTTPException(status_code=401, detail="wrong username or password")
 
 @router.options('/login')
-async def user_login(response: Response, user: schemas.UserLogin, db: Session=Depends(get_db)):
+async def user_login(user: schemas.UserLogin, db: Session=Depends(get_db)):
     db_user = crud.get_user_by_username(db=db, username=user.username)
     
     if (db_user and crud.validate_user_password(db=db, username=user.username, password=user.password)):
-        response.headers["Access-Control-Allow-Origin"] = '*'
         return sign_jwt(user.username)
     raise HTTPException(status_code=401, detail="wrong username or password")
 
 @router.get('/test')
-def get_test(response: Response):
-    response.headers["Access-Control-Allow-Origin"] = '*'
+def get_test():
     return {"response": "it works"}
