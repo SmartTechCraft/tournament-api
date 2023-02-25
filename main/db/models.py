7e102a2e2065fd50ceda79ecdfdaeea640e2dc11
DennisKretz <unrealfaceit2020@gmail.com>
2023-02-25 22:07:48 +0100

    add roles functionality

--- main/db/models.py
+++ main/db/models.py
@@ -8,7 +8,8 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(30), unique=True, index=True)
-    password = Column(String(45))
+    password = Column(String(45)) #NOTE: need to hash
     is_active = Column(Boolean, default=True)
+    role = Column(String(30), default="user")
 
     #items = relationship("Item", back_populates="owner")
--- main/db/schemas.py
+++ main/db/schemas.py
@@ -18,6 +18,7 @@ class User(UserBase):
 
     id: int
     is_active: bool
+    role: str
 
     class Config:
         
