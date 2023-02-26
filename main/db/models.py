5a5fc861342815bfb2cdf8aa9d69bda99263967e
DennisKretz <unrealfaceit2020@gmail.com>
2023-02-26 18:16:31 +0100

    add roles

--- main/db/models.py
+++ main/db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, JSON
 from sqlalchemy.orm import relationship
 from .mysql import Base
 
@@ -10,6 +10,18 @@ class User(Base):
     username = Column(String(30), unique=True, index=True)
     password = Column(String(45)) #NOTE: need to hash
     is_active = Column(Boolean, default=True)
-    role = Column(String(30), default="user")
+    role = Column(String(30), default="player")
 
     #items = relationship("Item", back_populates="owner")
+
+class Role(Base):
+
+    __tablename__ = "roles"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(64))
+    level = Column(Integer, default=1)
+    can_ban = Column(Boolean, default=False)
+    can_support = Column(Boolean, default=False)
+    can_manage = Column(Boolean, default=False)
+    can_view_routes = Column(JSON)
